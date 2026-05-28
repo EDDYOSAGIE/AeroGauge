6023d02dcb6c40856e9281a05db6c9dee1093e44
Precious <edionwe.precious@stu.cu.edu.ng>
2026-05-28 20:37:57 +0100

    Fix Vercel frontend deployment

--- frontend_Board/src/App.js
+++ frontend_Board/src/App.js
@@ -19,7 +19,7 @@ import {
 import axios from 'axios';
 
 
-const API_BASE_URL = 'http://127.0.0.1:5000';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:5000';
 const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID);
 
